8c603cf8dc83848c6b7ccdbd843b9c854119c73e
Edi Modrić <edi@netgen.hr>
2015-12-17 18:16:19 +0100

    Switch CS to PSR-2

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -8,12 +8,12 @@ use Symfony\Component\Config\Definition\ConfigurationInterface;
 class Configuration implements ConfigurationInterface
 {
     /**
-     * {@inheritDoc}
+     * {@inheritdoc}
      */
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root( 'netgen_content_type_list' );
+        $rootNode = $treeBuilder->root('netgen_content_type_list');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
--- DependencyInjection/NetgenContentTypeListExtension.php
+++ DependencyInjection/NetgenContentTypeListExtension.php
@@ -10,15 +10,15 @@ use Symfony\Component\DependencyInjection\Loader;
 class NetgenContentTypeListExtension extends Extension
 {
     /**
-     * {@inheritDoc}
+     * {@inheritdoc}
      */
-    public function load( array $configs, ContainerBuilder $container )
+    public function load(array $configs, ContainerBuilder $container)
     {
         $configuration = new Configuration();
-        $config = $this->processConfiguration( $configuration, $configs );
+        $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\YamlFileLoader( $container, new FileLocator( __DIR__ . '/../Resources/config' ) );
-        $loader->load( 'fieldtypes.yml' );
-        $loader->load( 'storage_engines.yml' );
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('fieldtypes.yml');
+        $loader->load('storage_engines.yml');
     }
 }

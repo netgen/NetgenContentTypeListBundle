0c0ceef4179a659095df7baa2840b9097602420d
Edi Modrić <edi.modric@gmail.com>
2019-07-09 09:51:36 +0200

    Remove empty config class

--- DependencyInjection/NetgenContentTypeListExtension.php
+++ DependencyInjection/NetgenContentTypeListExtension.php
@@ -17,9 +17,6 @@ class NetgenContentTypeListExtension extends Extension implements PrependExtensi
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
-
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('fieldtypes.yml');
         $loader->load('storage_engines.yml');

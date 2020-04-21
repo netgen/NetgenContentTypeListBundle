53450957ef19c25f72d7cd5387d0844e1334961a
Edi Modrić <edi.modric@gmail.com>
2020-04-21 12:10:53 +0200

    Use ezpublish DI extension to register config

--- DependencyInjection/NetgenContentTypeListExtension.php
+++ DependencyInjection/NetgenContentTypeListExtension.php
@@ -24,7 +24,7 @@ final class NetgenContentTypeListExtension extends Extension implements PrependE
     {
         $configFile = __DIR__ . '/../Resources/config/ezplatform.yaml';
         $config = Yaml::parse(file_get_contents($configFile));
-        $container->prependExtensionConfig('ezplatform', $config);
+        $container->prependExtensionConfig('ezpublish', $config);
         $container->addResource(new FileResource($configFile));
     }
 }
